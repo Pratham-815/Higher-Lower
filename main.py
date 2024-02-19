fd8c2206e63f19e936d4d89f91891585218c3940
Pratham Agarwal <prathamagarwal815@gmail.com>
2024-02-19 20:35:18 +0530

    Adding various functionalities to the game and make it repetetive

--- main.py
+++ main.py
@@ -26,3 +26,42 @@ def check_answer(guess, a_followers, b_followers):
     else:
         return guess == 'b'
     
+
+def game():
+    print(logo)
+    score = 0
+    game_should_continue = True
+    account_a = get_random_account()
+    account_b = get_random_account()
+
+    while game_should_continue:
+        
+        account_a = account_b
+        account_b = get_random_account()
+
+        while account_a == account_b:
+            account_b = get_random_account()
+
+        print(f"Compare A: {format_data(account_a)}")
+        print(vs)
+        print(f"Compare B: {format_data(account_b)}")
+
+        guess = input("Who has more follwers ? Type 'A' or 'B': ").lower()
+        a_follower_count = account_a['follower_count']
+        b_follower_count = account_b['follower_count']
+
+        is_correct = check_answer(guess, a_follower_count, b_follower_count)
+
+        os.system('cls')
+
+        print(logo)
+        
+        if is_correct:
+            score += 1
+            print(f"You are right...Current score: {score}")
+        else:
+            game_should_continue = False
+            print(f"Sorry...That's wrong...Final score: {score}")
+        
+
+game()
