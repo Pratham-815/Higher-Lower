744ad76eaf1931afba0f8ac6b1df9a1ec0398295
Pratham Agarwal <prathamagarwal815@gmail.com>
2024-02-19 20:24:44 +0530

    Create a function to check user guess

--- main.py
+++ main.py
@@ -18,3 +18,11 @@ def format_data(account):
     print(f"{name}, a {description}, from {country}")
 
 
+def check_answer(guess, a_followers, b_followers):
+    """Checks followers against user's guess and returns True if they got it right.Or False if they got it wrong."""
+
+    if a_followers > b_followers:
+        return guess == 'a'
+    else:
+        return guess == 'b'
+    
